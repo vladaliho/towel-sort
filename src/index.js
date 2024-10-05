3bc5dc0f5b5acb90bce8be9bd649f295e1a50dbe
vladaliho <vlada.liho@gmail.com>
2024-10-05 21:15:25 +0100

    fix: add fix for emty value

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-
 function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
 
-return matrix.reduce((acc, row, index) => {
-  if (index % 2 === 0) {
-    acc.push(...row);
-  } else {
-    acc.push(...row.reverse());
-  }
-  return acc;
-}, []);
+  return matrix.reduce((acc, row, index) => {
+    if (index % 2 === 0) {
+      acc.push(...row);
+    } else {
+      acc.push(...row.reverse());
+    }
+    return acc;
+  }, []);
 }
 
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
-
+module.exports = towelSort;
